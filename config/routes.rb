370517d05bf5c5ff9e446ee7eb9dc8b34315960e
lejour <29193321+lejour@users.noreply.github.com>
2023-05-29 07:15:22 +0000

    [Start] 4-22

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only:[:show, :edit, :update]
-  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+    resource :favorites, only:[:create, :destroy]
+    resources :post_comments, only:[:create, :destroy]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
